1cc7afcf66817c6d6406fd01958148bea8a5d62d
Andrew Lim <andy@custora.com>
2014-12-30 11:48:19 -0500

    Split speed tests into read+parse and generate

--- test/tc_speed.rb
+++ test/tc_speed.rb
@@ -7,24 +7,30 @@
 #  Copyright 2012 Gray Productions. All rights reserved.
 
 require 'minitest/autorun'
+require 'minitest/benchmark'
 require 'fastest_csv'
 require 'timeout'
 require 'csv'
 
 class TestCSVSpeed < Minitest::Test
 
-  PATH     = File.join(File.dirname(__FILE__), "test_data.csv")
-  BIG_DATA = "123456789\n" * 1024
+  PATH = File.join(File.dirname(__FILE__), "test_data.csv")
 
   def test_that_we_are_doing_the_same_work
+
     FastestCSV.open(PATH) do |csv|
       CSV.foreach(PATH) do |row|
-        assert_equal(row, csv.shift)
+        fastest_row = csv.shift
+        assert_equal(row, fastest_row)
+        nilled_row = row.map{|x| x == '' ? nil : x }  # FastestCSV does not quote empty elements, need to do this to force CSV to do the same
+        assert_equal(CSV.generate_line(nilled_row), FastestCSV.to_csv(fastest_row))
       end
     end
+
   end
 
-  def test_read_speed_vs_csv
+  def test_read_and_parse_speed_vs_csv
+
     csv_time = Time.now
     CSV.foreach(PATH) do |row|
       # do nothing, we're just timing a read...
@@ -37,14 +43,50 @@ class TestCSVSpeed < Minitest::Test
     end
     fastest_csv_time = Time.now - fastest_csv_time
 
-    puts "CSV read: #{csv_time}"
-    puts "FastestCSV read: #{fastest_csv_time}"
+    puts
+    puts "CSV read and parse: #{csv_time}"
+    puts "FastestCSV read and parse: #{fastest_csv_time}"
+    puts
 
     assert(fastest_csv_time < csv_time / 3)
 
   end
 
-  # We don't have bad CSV error checking in this gem as it currently stands
+  def test_generate_speed_vs_csv
+
+    csv_data = []
+    fastest_csv_data = []
+
+    CSV.foreach(PATH) do |row|
+      csv_data << row.map{|x| x == '' ? nil : x }  # don't include this conversion in the timing
+      fastest_csv_data << row
+    end
+
+    csv_time = Time.now
+    csv_data.each do |row|
+      CSV.generate_line(row)
+    end
+    csv_time = Time.now - csv_time
+
+    fastest_csv_time = Time.now
+    fastest_csv_data.each do |row|
+      FastestCSV.to_csv(row)
+    end
+    fastest_csv_time = Time.now - fastest_csv_time
+
+    puts
+    puts "CSV generate: #{csv_time}"
+    puts "FastestCSV generate: #{fastest_csv_time}"
+    puts
+
+    assert(fastest_csv_time < csv_time / 3)
+
+  end
+
+  # BIG_DATA = "123456789\n" * 1024
+
+  # We don't have bad CSV error checking in this gem as it currently stands, so
+  # this part is commented out
 
   # def test_the_parse_fails_fast_when_it_can_for_unquoted_fields
   #   assert_parse_errors_out('valid,fields,bad start"' + BIG_DATA)
